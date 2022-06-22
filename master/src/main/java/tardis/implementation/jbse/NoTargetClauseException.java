eaf00e6d3571292f109bdfb1f3835f18a5aeb678
alessandromanetti97 <42972467+alessandromanetti97@users.noreply.github.com>
2022-06-22 16:40:55 +0200

    Create NoTargetClauseException.java
    Exception created for PathConditionTracker.java

---
+++ master/src/main/java/tardis/implementation/jbse/NoTargetClauseException.java
@@ -0,0 +1,13 @@
+package tardis.implementation.jbse;
+
+public class NoTargetClauseException extends RuntimeException {
+
+	public NoTargetClauseException() {
+		super();
+	}
+
+	public NoTargetClauseException(String message) {
+		super(message);
+	}
+	
+}
