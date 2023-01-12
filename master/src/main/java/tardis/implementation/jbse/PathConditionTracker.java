0227b8659f490cacdf60bfbe5e62c5186a526287
alessandromanetti97 <42972467+alessandromanetti97@users.noreply.github.com>
2023-01-12 17:42:17 +0100

    Update PathConditionTracker.java

--- master/src/main/java/tardis/implementation/jbse/PathConditionTracker.java
+++ master/src/main/java/tardis/implementation/jbse/PathConditionTracker.java
@@ -1,66 +1,115 @@
 package tardis.implementation.jbse;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import jbse.bc.ClassFile;
+import jbse.bc.ClassFileJavassist;
+import jbse.bc.Signature;
 import jbse.mem.Clause;
 import jbse.mem.ClauseAssumeClassInitialized;
 import jbse.mem.ClauseAssumeClassNotInitialized;
+import jbse.mem.Frame;
 import jbse.mem.State;
 import jbse.mem.State.Phase;
+import jbse.mem.exc.FrozenStateException;
+import jbse.mem.exc.ThreadStackEmptyException;
 import jbse.tree.StateTree.BranchPoint;
 
 public class PathConditionTracker {
 	
 	//MYCHANGES
-			private Map <Clause, Integer> livelliDiAnnidamento = new HashMap<>();
+	
+	private Map <Clause, List<Signature>> stackPerClausola = new HashMap<>();
 			private int currentPathConditionSize;
-					
+
 			public int getLivelloDiAnnidamento(Clause c) {
-				if (livelliDiAnnidamento.containsKey(c)) {
-					return livelliDiAnnidamento.get(c);
+				if (stackPerClausola.containsKey(c)) {
+					return stackPerClausola.get(c).size();  //.get(c).size();
 				} else {
-					throw new NoTargetClauseException();
+					return 0;
+					//throw new NoTargetClauseException(); 
 				}		
 			}
 			
-			//CHANGES 14/06
-			public Map <Clause, Integer> getLivelliDiAnnidamento(){
-				return this.livelliDiAnnidamento;
+			public String getClasse(Clause c) {
+				if (stackPerClausola.containsKey(c)) {
+					List<Signature> stackData = stackPerClausola.get(c);
+					if (stackData.size() > 0) {
+						return stackData.get(stackData.size() - 1).getClassName();
+					}
+				} 
+				return null; 
+				//throw new NoTargetClauseException();		
 			}
-
+			
+			//* DECEMBER CHANGES 
+			public String getMetodo(Clause c) {
+				if (stackPerClausola.containsKey(c)) {
+					List<Signature> stackData = stackPerClausola.get(c);
+					return stackData.get(stackData.size() - 1).getName();
+				} else {
+					return null;
+					//throw new NoTargetClauseException();
+				}		
+			}
+			
+			public List <Signature> getListaSignature (Clause c){
+				if (stackPerClausola.containsKey(c)) {
+					return stackPerClausola.get(c);
+				} else {
+					return null;
+				}
+			}
+			//ENDS*/
+			
+			
 			public void atStepPre(State currentState) {
-				currentPathConditionSize = currentState.getPathCondition().size();
+				currentPathConditionSize = currentState.getPathCondition().size(); 
 				
 			}
 
-			public void atStepPost(State currentState) {
+			public void atStepPost(State currentState) throws ThreadStackEmptyException, FrozenStateException { //Handle exception!!!!!!
 				if (currentState.phase() == Phase.POST_INITIAL) {
 					
 					int newClauses = currentState.getPathCondition().size() - currentPathConditionSize;
 					List<Clause> pc = currentState.getPathCondition();
-					for (int i=0; i<newClauses; i++) {
+					
+					for (int i = 0; i < newClauses; i++) {
 						Clause c = pc.get(pc.size() - 1 - i);
 						if (!(c instanceof ClauseAssumeClassInitialized) && !(c instanceof ClauseAssumeClassNotInitialized)) {
-							livelliDiAnnidamento.put(c , currentState.getStackSize() - 1);
-							//System.out.println(livelliDiAnnidamento);
+							stackPerClausola.put(c, getSignatures(currentState));
+							//metodiDelleClausole.put(c, currentState.getCurrentMethodSignature().getName());	//METODI
+							//classiDelleClausole.put(c, currentState.getCurrentClass().getClassName());		//CLASSI
 						}
 					}
 				}				
 			} 
 			
-			public void atBacktrackPost(State currentState) {
+			private List<Signature> getSignatures(State currentState) throws FrozenStateException {
+				ArrayList<Signature> stackData = new ArrayList<>();
+				List<Frame> stack = currentState.getStack();
+				for (Frame f: stack) {
+					stackData.add(f.getMethodSignature()); 
+				}
+				return stackData;
+			}
+
+			public void atBacktrackPost(State currentState) throws ThreadStackEmptyException, FrozenStateException { //Handle exception!!!!!
 				if (currentState.phase() == Phase.POST_INITIAL) {
 					List<Clause> pc = currentState.getPathCondition();
 					for (int i = pc.size() - 1; i >= 0; i--) {
 						Clause c = pc.get(i);
-						if (livelliDiAnnidamento.containsKey(c)) {
+						if (stackPerClausola.containsKey(c)  /*metodiDelleClausole.containsKey(c)*/) {
 							break;
 						}
 						if (!(c instanceof ClauseAssumeClassInitialized) && !(c instanceof ClauseAssumeClassNotInitialized)) {
-							livelliDiAnnidamento.put(c , currentState.getStackSize() - 1);
-								//System.out.println(livelliDiAnnidamento);
+							stackPerClausola.put(c, getSignatures(currentState));
+							//livelliDiAnnidamento.put(c , currentState.getStackSize() - 1);
+							//metodiDelleClausole.put(c, currentState.getCurrentMethodSignature().getName()); //METODI
+							//classiDelleClausole.put(c, currentState.getCurrentClass().getClassName()); //CLASSI
 						}	
 					}	
 				}
